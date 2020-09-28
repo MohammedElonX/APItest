56ee8909d71fd3501e064f72e22ac7f86dee9131
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-09-28 15:09:53 +0100

    Testig the HusmoData API

--- app/src/main/java/com/example/apitest/ScrollingActivity.java
+++ app/src/main/java/com/example/apitest/ScrollingActivity.java
@@ -1,16 +1,27 @@
 package com.example.apitest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class ScrollingActivity extends AppCompatActivity {
 
+    private OkHttpClient client;
+    private TextView textV;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +33,65 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Not Available", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        textV = findViewById(R.id.textVi);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                getResponse();
+            }
+        });
+    }
+
+    private void getResponse() {
+        String value = "Token b3aacd45d256fb778973feb6aa8839c1fcdb4f13";
+        String token = (String) Base64.encodeToString(value.getBytes(StandardCharsets.UTF_8), Base64.DEFAULT);
+        client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://www.husmodata.com/api/user/")
+                .method("GET", null)
+                .addHeader("Authorization", value)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        Request req = new Request.Builder()
+                .url("https://www.husmodata.com/api/data/")
+                .method("GET", null)
+                .addHeader("Authorization", value)
+                .build();
+
+        client.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textV.setText("Error fetching data..!");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            textV.setText("Output: \n" + response.body().string() + "\n" + response.message());
+                        } catch (IOException e) {
+                            textV.setText("Nothing found...!!!");
+                        }
+                    }
+                });
+
             }
         });
     }
@@ -44,6 +112,7 @@ public class ScrollingActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent(this, monnifyApi.class);
             return true;
         }
         return super.onOptionsItemSelected(item);
